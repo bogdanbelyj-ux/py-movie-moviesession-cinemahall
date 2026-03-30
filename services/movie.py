e2df786a7c8301b3d0336ec5fac33fa191d60e92
Bohdan <bogdanbelyj@gmail.com>
2026-03-29 20:56:02 -0600

    Solution 3

--- services/movie.py
+++ services/movie.py
@@ -8,11 +8,9 @@ def get_movies(
         actors_ids: list[int] = None
 ) -> QuerySet:
     qs = Movie.objects.all()
-    if genres_ids and actors_ids:
-        qs = qs.filter(genres__id__in=genres_ids, actors__id__in=actors_ids)
-    elif genres_ids:
+    if genres_ids:
         qs = qs.filter(genres__id__in=genres_ids)
-    elif actors_ids:
+    if actors_ids:
         qs = qs.filter(actors__id__in=actors_ids)
     return qs.distinct()
 
